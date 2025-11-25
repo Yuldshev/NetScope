a84f002c449d6287d39d48f163c750f5fefd42ff
yuldshev <ikrom921@gmail.com>
2025-11-25 11:09:12 +0500

    Initial Commit

---
+++ NetScope/NetScopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetScopeApp.swift
+//  NetScope
+//
+//  Created by T on 25/11/25.
+//
+
+import SwiftUI
+
+@main
+struct NetScopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
